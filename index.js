3c4f743c49643df2caf7046b6f393a9f04efc920
Duncan Birtwistle <duncan.birtwistle@googlemail.com>
2016-04-17 21:42:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ controller.hears(['hello', 'hi'], ['ambient'], function (bot, message) {
   bot.reply(message, 'Hello.')
 })
 
+controller.hears(['can I have some water'], ['ambient'], function (bot, message) {
+  bot.reply(message, 'Hello.')
+})
+
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'Hello.')
   bot.reply(message, 'It\'s nice to talk to you directly.')
