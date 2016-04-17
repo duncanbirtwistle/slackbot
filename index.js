834d4f8274fce99e620f387292cadae5eb5bf1f2
Duncan Birtwistle <duncan.birtwistle@googlemail.com>
2016-04-18 00:57:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,22 +22,11 @@ bot.startRTM(function (err, bot, payload) {
 
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
             bot.reply(message, 'Hello ' + user.name + '!!');
         } else {
-            bot.reply(message, 'Hello.');
+            bot.reply(message, 'Hello. What is your name?');
         }
     });
 });
