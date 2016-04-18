707c02e4d652a6f4b8795aba9bed480f2c2eb640
Duncan Birtwistle <duncan.birtwistle@googlemail.com>
2016-04-18 02:42:18 +0100

    read

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ controller.hears(['can I have some water'], ['direct_message'], function (bot, m
 
 //CONVERSATION
 
-controller.hears(['pizzatime'],['direct_message'],function(bot,message) {
+controller.hears(['read'],['direct_message'],function(bot,message) {
   bot.startConversation(message, askReading);
 });
 
